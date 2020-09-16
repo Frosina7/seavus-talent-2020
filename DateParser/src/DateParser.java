db78f2c219596f94387c061286f8fb0c9a5f1a46
Frosina7 <gicevafrose@gmail.com>
2020-09-16 10:13:39 +0200

    The code is reformatted and imports are erased

--- DateParser/src/DateParser.java
+++ DateParser/src/DateParser.java
@@ -1,20 +1,13 @@
 import java.util.Scanner;
 
-import static java.lang.System.*;
-
-import java.io.IOException;
-
 public class DateParser {
 
-    public static void main(String []args)throws IOException
-    {
-        String s=new Scanner(System.in).nextLine();
+    public static void main(String[] args) {
 
-        String[] s1=s.split("/");
-        
-        System.out.println(s.substring(0,2)+" "+s.substring(3,5)+" "+s.substring(6,s.length()));
-         
- 
+        String s = new Scanner(System.in).nextLine();
+
+        System.out.println(s.substring(0, 2) + " " + s.substring(3, 5) + " " + s.substring(6, s.length()));
+
 
     }
 }
