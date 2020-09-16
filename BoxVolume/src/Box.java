39db69cb971081b1796499d6c88e43c1f9fa6317
Frosina7 <gicevafrose@gmail.com>
2020-09-16 10:50:36 +0200

    Added new BoxVolume homework

---
+++ BoxVolume/src/Box.java
@@ -0,0 +1,45 @@
+public class Box {
+
+    private Integer height;
+    private Integer width;
+    private Integer depth;
+
+    public Integer getHeight() {
+        return height;
+    }
+
+    public void setHeight(Integer height) {
+        this.height = height;
+    }
+
+    public Integer getWidth() {
+        return width;
+    }
+
+    public void setWidth(Integer width) {
+        this.width = width;
+    }
+
+    public Integer getDepth() {
+        return depth;
+    }
+
+    public void setDepth(Integer depth) {
+        this.depth = depth;
+    }
+
+    public Box() {
+    }
+
+    ;
+
+    public Box(Integer height, Integer width, Integer depth) {
+        this.height = height;
+        this.width = width;
+        this.depth = depth;
+    }
+
+    public Integer findVolume(Integer height, Integer width, Integer depth) {
+        return height * width * depth;
+    }
+}
