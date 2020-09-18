6165e79f76c4aeac4193fa074ee5ea69926774ee
Frosina7 <gicevafrose@gmail.com>
2020-09-18 09:33:22 +0200

    Call of findVolume is changed

--- BoxVolume/src/Application.java
+++ BoxVolume/src/Application.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Application extends Box {
+public class Application {
     public static void main(String[] args) {
         int h = new Scanner(System.in).nextInt();
         int w = new Scanner(System.in).nextInt();
@@ -8,6 +8,8 @@ public class Application extends Box {
 
         Box box = new Box(h, w, d);
 
-        System.out.println(box.findVolume(h, w, d));
+        System.out.println(box.findVolume());
     }
+
+
 }
