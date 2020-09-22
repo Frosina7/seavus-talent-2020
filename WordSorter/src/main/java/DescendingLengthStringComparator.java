037d27ae99f0a3316f59931dbd169d051f904687
Frosina7 <gicevafrose@gmail.com>
2020-09-22 17:08:27 +0200

    Method is changed

--- WordSorter/src/main/java/DescendingLengthStringComparator.java
+++ WordSorter/src/main/java/DescendingLengthStringComparator.java
@@ -4,6 +4,6 @@ public class DescendingLengthStringComparator implements Comparator<String> {
     @Override
     public int compare(String s1, String s2) {
 
-        return (s1.length()-s2.length())*(-1);
+        return s2.length()-s1.length();
     }
 }
