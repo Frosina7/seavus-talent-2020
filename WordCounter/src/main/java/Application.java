cb71a51d21ca53ea5dda85ffbfe87911f68cb70e
Frosina7 <gicevafrose@gmail.com>
2020-09-22 09:41:51 +0200

    Method name is changed

--- WordCounter/src/main/java/Application.java
+++ WordCounter/src/main/java/Application.java
@@ -6,7 +6,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Application {
-    public static void getChar(String s) {
+    public static void findWord(String s) {
 
         HashMap<String, Integer> table = new HashMap<>();
 
@@ -33,7 +33,7 @@ public class Application {
         File file = new File("file.txt");
         String string = FileUtils.readFileToString(file,"UTF-8");
         String result = string.replaceAll("[^a-zA-z]", " ");
-        getChar(result);
+        findWord(result);
     }
 
 
